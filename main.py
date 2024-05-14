b69f64f262798a24aad783a8918df309b22d6a93
awais0731 <awaisraza0731@gmail.com>
2024-05-14 15:38:38 +0500

    Add loggin and Exception files

---
+++ main.py
@@ -0,0 +1,16 @@
+from sensor.exception import SensorException
+from sensor.logger import logging
+import sys, os
+
+def test_exception():
+    try:
+        logging.info("there will be error that not divisible by zero")
+        a = 1/0
+    except Exception as e:
+        raise SensorException(e,sys)
+
+if __name__ == "__main__":
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
