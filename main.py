b37a050f4b820afd862ce5f825fc30090a5dacca
awais0731 <awaisraza0731@gmail.com>
2024-05-31 13:03:26 +0500

    code save till data validation

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from sensor.exception import SensorException
 from sensor.logger import logging
-import sys, os
-
-def test_exception():
-    try:
-        logging.info("there will be error that not divisible by zero")
-        a = 1/0
-    except Exception as e:
-        raise SensorException(e,sys)
+import sys, os 
+from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.utils2 import dump_csv_file_to_mongodb_collection
 
 if __name__ == "__main__":
-    try:
-        test_exception()
-    except Exception as e:
-        print(e)
+
+    # file_path="aps_failure_training_set1.csv"
+    # database_name="ineuron"
+    # collection_name ="sensor"
+    # dump_csv_file_to_mongodb_collection(file_path,database_name,collection_name)
+
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
