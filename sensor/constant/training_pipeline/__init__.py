eaa3b09fba4d120bfcc7088d3228dfedf4730831
awais0731 <awaisraza0731@gmail.com>
2024-06-04 12:09:44 +0500

    Add code of model evaluation

--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -1,25 +1,24 @@
-import os 
+import os
 
+SAVED_MODEL_DIR =os.path.join("saved_models")
+# defining common constant variable for training pipeline
 TARGET_COLUMN = "class"
-PIPELINE_NAME = "sensor"
-ARTIFACT_DIR = "artifact"
-FILE_NAME = "sensor.csv"
+PIPELINE_NAME: str = "sensor"
+ARTIFACT_DIR: str = "artifact"
+FILE_NAME: str = "sensor.csv"
 
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
-
 PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 MODEL_FILE_NAME = "model.pkl"
-
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 SCHEMA_DROP_COLS = "drop_columns"
 
 
 """
- data  ingestion realted constant values 
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
-
 DATA_INGESTION_COLLECTION_NAME: str = "sensor"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
@@ -53,3 +52,15 @@ MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+
+"""
+Model Trainer ralated constant start with MODE TRAINER VAR NAME
+"""
+MODEL_EVALUATION_DIR_NAME: str = "model_evaluation"
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_EVALUATION_REPORT_NAME= "report.yaml"
+
+
+MODEL_PUSHER_DIR_NAME = "model_pusher"
+MODEL_PUSHER_SAVED_MODEL_DIR = SAVED_MODEL_DIR
